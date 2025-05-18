71c2d8940c343b2a2f76e40d19770c2c5844d625
bannle <maryfloresdke@gmail.com>
2025-05-17 19:42:07 -0600

    feat: añadir animaciones a los modales

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+
+Route::get('gestion_empresas', function () {
+    return view('gestion_empresas');
+});
+
+Route::get('listado_convenios', function () {
+    return view('listado_convenios');
+});
