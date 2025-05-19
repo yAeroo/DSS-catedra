fac12e5b5d64c4c4d13a91c21d4c3795892e4083
yAeroo <ehamaya404@gmail.com>
2025-05-19 01:19:58 -0600

    setup: Añadido jqueryMask

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@ import './bootstrap';
 import jQuery from 'jquery';
 window.$ = jQuery;
 
+import 'jquery-mask-plugin';
+
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 window.iziToast = iziToast;
