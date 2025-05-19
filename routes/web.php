ae36b38bcd20da0f8e7c35bc69cb7fd3e76ab07f
FloppaCoder <marcelo.menjivar2006@gmail.com>
2025-05-19 00:59:25 -0600

    feat: Actualizar y Deshabilitar Tipos de Empresas

--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,15 @@ Route::get('/', function () {
 
 //Gestion de Empresas
 Route::get('gestion_empresas', [TipoEmpresaController::class, 'index'])->name('tipos-empresa.index');
-/* Route::get('gestion_empresas', function () {
-    return view('gestion_empresas');
-})->name('gestion_empresas'); */
 
 //Create Tipo de Empresa (store)
 Route::post('gestion_empresas', [TipoEmpresaController::class, 'store'])->name('tipos-empresa.store');
 
+// Update Tipo de Empresa
+Route::put('gestion_empresas/{id}', [TipoEmpresaController::class, 'update'])->name('tipos-empresa.update');
+
+// Delete Tipo de Empresa
+Route::delete('gestion_empresas/{id}', [TipoEmpresaController::class, 'destroy'])->name('tipos-empresa.destroy');
 
 //Gestion de Convenios
 Route::get('listado_convenios', function () {
